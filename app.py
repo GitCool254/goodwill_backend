6c51ececb80af6cd964958e848410856f6694c00
GitCool254 <goodwillstores35@gmail.com>
2026-01-08 09:47:47 +0300

    Allow Netlify frontend in CORS

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ CORS(
     app,
     resources={
         r"/generate_ticket": {
-            "origins": ["https://goodwill-raffle-store-raffle-store.onrender.com"]
+            "origins": ["https://goodwill-raffle-store-raffle-store.onrender.com", "https://goodwillrafflestore.netlify.app"]
         }
     }
 )
