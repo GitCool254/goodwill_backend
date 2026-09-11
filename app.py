a8920d79920f22a16fbcd55ed49601a418767fbe
GitCool254 <goodwillstores35@gmail.com>
2026-09-11 16:31:04 +0300

    All

--- app.py
+++ app.py
@@ -150,7 +150,7 @@ CORS(
 )
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-TEMPLATE_PATH = os.path.join(BASE_DIR, "Goodwillstores_Ticket_Template8.pdf")
+TEMPLATE_PATH = os.path.join(BASE_DIR, "Goodwillstores_Ticket_Template9.pdf")
 # Service account key (already in your Termux setup)
 GSHEET_KEY_FILE = os.path.join(BASE_DIR, "goodwill-backend.json")
 GSHEET_ID = os.environ.get("GSHEET_ID")
